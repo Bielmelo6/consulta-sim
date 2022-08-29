d20069de6f92a685553cfb5c409052d649340a52
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-08-29 10:51:21 -0300

    ultimos testes do model paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -2,19 +2,30 @@ require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
   test 'Criando paciente com campos preenchidos corretamente' do
-    paciente = Paciente.new name: 'Gabriel Melo', birthday: Date.parse("Dec 6 1999"),
+    paciente = Paciente.new name: 'Gabriel Melo de Menezes', birthday: Date.parse("Dec 6 1999"),
                             cpf: '129342224-03', email: 'gabrielmelodemenezes@gmail.com'
     assert paciente.save
   end
 
   test 'Criando paciente com os campos do endereco preenchidos' do
-    paciente = Paciente.new name: 'Gabriel Melo', birthday: Date.parse("Dec 6 1999"),
+    paciente = Paciente.new name: 'Gabriel Melo de Menezes', birthday: Date.parse("Dec 6 1999"),
                             cpf: '129342224-03', email: 'gabrielmelodemenezes@gmail.com'
     endereco = Endereco.new city: 'Garanhuns', neighborhood: 'Boa Vista',
                             street_avenue: 'Rua dos Guararapes', complement: 'ap 7',
                             cep: '55000-00'
     endereco.paciente = paciente.id
     assert paciente.save
+  end
+
+  test 'Criando paciente com a data de nascimento invalida' do
+    paciente = Paciente.new name: 'Gabriel Melo', birthday: Date.parse("Dec 6 2025"),
+                            cpf: '129342224-03', email: 'gabrielmelodemenezes@gmail.com'
+    assert_not paciente.save
+  end
+
+  test 'Criando paciente sem os campos preenchidos' do
+    paciente = Paciente.new
+    assert_not paciente.save
   end
   # test "the truth" do
   #   assert true
