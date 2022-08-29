02bfe82c0bb48f0c67bc890614fc41222ffb026d
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-08-29 11:57:40 -0300

    segundo cenario paciente

--- features/step_definitions/paciente_steps.rb
+++ features/step_definitions/paciente_steps.rb
@@ -3,7 +3,7 @@ Given('Estou no formulario de cadastro do paciente') do
   expect(page).to have_current_path('/pacientes/new')
 end
 
-When('eu preencho os campos name {string} birthday {string} cpf {string} email {string} city {string} neighborhood {string} street_avenue {string} complement {string} cep {string} e aperto no botao de cadastrar') do | name, birthday, cpf, email, city, neighborhood, street_avenue, complement, cep|
+When('eu preencho os campos name {string} birthday {string} cpf {string} email {string} city {string} neighborhood {string} street_avenue {string} complement {string} cep {string} e aperto no botao de cadastrar') do |name, birthday, cpf, email, city, neighborhood, street_avenue, complement, cep|
   fill_in 'paciente[name]', :with => name
   fill_in 'paciente[birthday]', :with => birthday
   fill_in 'paciente[cpf]', :with => cpf
@@ -20,4 +20,34 @@ Then('eu visualizo que o cadastro com o name {string} foi realizado com sucesso'
   expect(page).to have_content(name)
   expect(page).to have_current_path(pacientes_path + '/' +
                                       Paciente.last.id.to_s)
+end
+
+Given('Estou na pagina index dos pacientes') do
+  visit 'pacientes'
+  expect(page).to have_current_path('/pacientes')
+end
+
+And('o paciente de name {string} birthday {string} cpf {string} email {string} city {string} neighborhood {string} street_avenue {string} complement {string} cep {string} existe') do |name, birthday, cpf, email, city, neighborhood, street_avenue, complement, cep|
+  visit 'pacientes/new'
+  fill_in 'paciente[name]', :with => name
+  fill_in 'paciente[birthday]', :with => birthday
+  fill_in 'paciente[cpf]', :with => cpf
+  fill_in 'paciente[email]', :with => email
+  fill_in 'paciente[city]', :with =>city
+  fill_in 'paciente[neighborhood]', :with => neighborhood
+  fill_in 'paciente[street_avenue]', :with => street_avenue
+  fill_in 'paciente[complement]', :with => complement
+  fill_in 'paciente[cep]', :with => cep
+  click_button 'Create Paciente'
+  expect(page).to have_content(name)
+end
+
+When('eu aperto no botao para remover o paciente de nome {string}') do |name|
+  expect(page).to have_content(name)
+  click_button "Destroy this Paciente"
+end
+
+Then('eu visualizo que o paciente com nome {string} foi removido com sucesso') do |name|
+  expect(page).to have_no_content(name)
+  expect(page).to have_current_path('/pacientes')
 end
--- features/support/paciente.feature
+++ features/support/paciente.feature
@@ -7,3 +7,9 @@ Feature:
     Given Estou no formulario de cadastro do paciente
     When eu preencho os campos name 'Gabriel Melo de Menezes' birthday '1999 12 06' cpf '129342224-03' email 'gabrielmelodemenezes@gmail.com' city 'Garanhuns' neighborhood 'Boa Vista' street_avenue 'Rua dos Guararapes' complement 'ap 7' cep '55000-00' e aperto no botao de cadastrar
     Then eu visualizo que o cadastro com o name 'Gabriel Melo de Menezes' foi realizado com sucesso
+
+  Scenario: remover paciente
+    Given Estou na pagina index dos pacientes
+    And o paciente de name 'Arthur Luis de Melo' birthday '2010 11 03" cpf '129342224-03' email 'arthurtuteres@gmail.com' city 'Garanhuns' neighborhood 'Boa Vista' street_avenue 'Rua dos Guararapes' complement 'ap 7' cep '55000-00' existe
+    When eu aperto no botao para remover o paciente de nome 'Arthur Luis de Melo'
+    Then eu visualizo que o paciente com nome 'Arthur Luis de Melo' foi removido com sucesso
