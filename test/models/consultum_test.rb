a3c9d6823d044676748d9dac7940d09a3f434c92
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-08-29 10:12:18 -0300

    ultimos testes do model consultum

--- test/models/consultum_test.rb
+++ test/models/consultum_test.rb
@@ -16,6 +16,28 @@ class ConsultumTest < ActiveSupport::TestCase
     consulta = Consultum.new date: DateTime.now, time: '15:00', paciente_id: paciente.id, medico_id: medico.id
     assert consulta.save
   end
+
+  test 'Criando consulta com os campos do medicos preenchidos errado' do
+    paciente = Paciente.new nome: 'Gabriel Melo de Menezes', birthday: DateTime.now,
+                            cpf: '12934222403', email: 'gabrielmelodemenezes@gmail.com'
+    assert paciente.save
+    medico = Medico.new name: 'Arthur Luís', cpf: '123456789-10', email: 'arthurtuteres@gmail.com',
+                        speciality: 'Ortopedista', crm: '12'
+    assert medico.save
+    consulta = Consultum.new date: DateTime.now, time: '15:00', paciente_id: paciente.id, medico_id: medico.speciality
+    assert_not consulta.save
+  end
+
+  test 'Criando consulta com os campos do pacientes preenchidos errado' do
+    paciente = Paciente.new name: 'Gabriel Melo de Menezes', birthday: DateTime.now,
+                            cpf: '129342224-03', email: 'gabrielmelodemenezes@gmail.com'
+    assert paciente.save
+    medico = Medico.new name: 'Arthur Luís', cpf: '123456789-10', email: 'arthurtuteres@gmail.com',
+                        speciality: 'Ortopedista', crm: 'CRM/PE 123456'
+    assert medico.save
+    consulta = Consultum.new date: DateTime.now, time: '15:00', paciente_id: paciente.cpf, medico_id: medico.id
+    assert_not consulta.save
+  end
   # test "the truth" do
   #   assert true
   # end
