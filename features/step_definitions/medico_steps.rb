d80d50c046bc882138e14f003455e96a4e19fa9d
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-08-29 11:51:46 -0300

    segundo cenario medico

--- features/step_definitions/medico_steps.rb
+++ features/step_definitions/medico_steps.rb
@@ -3,7 +3,7 @@ Given('Estou na pagina index dos medicos') do
   expect(page).to have_current_path('/medicos')
 end
 
-Given('o medico de name {string} cpf {string} email {string} speciality {string} crm {string} existe') do |name, cpf, email, speciality, crm|
+And('o medico de name {string} cpf {string} email {string} speciality {string} crm {string} existe') do |name, cpf, email, speciality, crm|
   visit 'medicos/new'
   fill_in 'medico[name]', :with => name
   fill_in 'medico[cpf]', :with => cpf
@@ -22,4 +22,29 @@ end
 Then('eu visualizo que o medico com nome {string} foi removido com sucesso') do |name|
   expect(page).to have_no_content(name)
   expect(page).to have_current_path('/medicos')
+end
+
+Given('Estou no formulario de cadastro do medico') do
+  visit 'medicos/new'
+  expect(page).to have_current_path('/medicos/new')
+end
+
+And('eu preencho os campos name {string} cpf {string} email {string} speciality {string} crm {string} e aperto no botao de cadastrar') do | name, cpf, email, speciality, crm|
+  fill_in 'medico[name]', :with => name
+  fill_in 'medico[cpf]', :with => cpf
+  fill_in 'medico[email]', :with => email
+  fill_in 'medico[speciality]', :with => speciality
+  fill_in 'medico[crm]', :with => crm
+  click_button 'Create Medico'
+end
+
+When('eu aperto no botao para cadastrar o medico de nome {string}') do |name|
+  expect(page).to have_content(name)
+  click_button "Create Medico"
+end
+
+Then('eu visualizo que o cadastro com o name {string} foi realizado com sucesso') do |name|
+  expect(page).to have_content(name)
+  expect(page).to have_current_path(medicos_path + '/' +
+                                      Medico.last.id.to_s)
 end
--- features/support/medico.feature
+++ features/support/medico.feature
@@ -8,3 +8,9 @@ Feature:
     And o medico de name 'Gabriel Melo de Menezes' cpf '129342224-03' email 'gabrielmelodemenezes@gmail.com' speciality 'Cardiologista' crm 'CRM/PE 123456' existe
     When eu aperto no botao para remover o medico de nome 'Gabriel Melo de Menezes'
     Then eu visualizo que o medico com nome 'Gabriel Melo de Menezes' foi removido com sucesso
+
+  Scenario: cadastrar medico
+    Given Estou no formulario de cadastro do medico
+    And o medico de name 'Arthur Luis de Melo' cpf '123456789-10' email 'arthurtuteres@gmail.com' speciality 'Cardiologista' crm 'CRM/PE 123456' existe
+    When eu aperto no botao para cadastrar o medico de nome 'Arthur Luis de Melo'
+    Then eu visualizo que o cadastro com o name 'Arthur Luis de Melo' foi realizado com sucesso
