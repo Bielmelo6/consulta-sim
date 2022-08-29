84d12e76c48e5a57a6f31929972ef0b1f1b600c6
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-08-29 10:15:48 -0300

    primeiros testes do model medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
+  test 'Criando medico com campos preenchidos corretamente' do
+    medico = Medico.new nome: 'Gabriel Melo', cpf: '129342224-03', email: 'gabrielmelodemenezes@gmail.com',
+                        especialidade: 'Ortopedista', crm: 'CRM/PE 123456'
+    assert medico.save
+  end
+
+  test 'Criando medico com o campo crm invalido' do
+    medico = Medico.new nome: 'Gabriel Melo', cpf: '129342224-03', email: 'gabrielmelodemenezes@gmail.com',
+                        especialidade: 'Ortopedista', crm: 'CRMPE 12'
+    assert_not medico.save
+  end
   # test "the truth" do
   #   assert true
   # end
